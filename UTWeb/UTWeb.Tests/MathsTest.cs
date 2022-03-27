01a53893fc93f541c34fb62a1e97b1e28b52a16e
Srini <Srini@DESKTOP-N9QHGQC>
2022-03-27 12:46:39 +0530

    code added

---
+++ UTWeb/UTWeb.Tests/MathsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTWeb.Tests
+{
+    [TestFixture]
+    class MathsTest
+    {
+        [Test]
+        public void AddTestPostiveRes()
+        {
+            Maths maths = new Maths();
+
+            var res = maths.Add(10, 30);
+
+            Assert.AreEqual(40, res);
+        }
+    }
+}
